4915f4978234e1cfcae6f8e1d4a8433f6d007a49
James Snee <jamessnee1@gmail.com>
2015-01-24 14:28:29 +1100

    remove function prototype

--- stack.h
+++ stack.h
@@ -18,5 +18,4 @@ void stack_push(stack_s * stack, char item);
 char stack_pop(stack_s * stack);
 int stack_is_empty(stack_s * stack);
 void stack_free(stack_s * stack);
-int run_stack(stack_s *stack, char * input);
 
